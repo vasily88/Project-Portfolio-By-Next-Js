9d20f05f9523327bb146c2dfc1880b5d038d45a3
vasily88 <vasek88@gmail.com>
2024-07-06 12:50:40 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
